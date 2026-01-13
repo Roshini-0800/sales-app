41e86ab3ff4a1518deff7fe3e31a254b7bbfb7c6
Roshini <roshini-email@example.com>
2026-01-13 17:02:33 +0530

    Deploy RO Sales App to ECS

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "ro-secret-key"   # required for login session
 
-@app.route("/")
-def health():
-    return "Hi Roshini, Sales Application is running successfully 🚀"
+# Dummy login credentials
+USERNAME = "admin"
+PASSWORD = "admin123"
 
-@app.route("/sales")
-def sales():
-    return jsonify({
-        "application": "Sales App",
-        "total_sales": 150000,
-        "currency": "INR",
-        "region": "India"
-    })
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == USERNAME and password == PASSWORD:
+            session["user"] = username
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("login.html", error="Invalid credentials")
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect(url_for("login"))
+    return render_template("index.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
